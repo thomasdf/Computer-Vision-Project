d83a5f00cf9fba7a17f962a917013bbf9f6290e1
Ole Viktor Ravna <olevra@stud.ntnu.no>
2017-04-18 02:24:30 +0200

    Expand Img class

--- objectsCrowdAI/Image.py
+++ objectsCrowdAI/Image.py
@@ -3,10 +3,16 @@ from PIL import Image
 
 
 class Img:
-
 	def __init__(self, path):
 		self.image = Image.open(path)
 		self.arr2d = np.array(self.image)
 		self.shape = self.arr2d.shape
 		self.arr1d = self.arr2d.ravel()
+
+	def show(self):
+		self.image.show()
+
+	def crop(self, xmin: int, ymin: int, xmax: int, ymax: int):
+		self.croped = self.image.crop((xmin, ymin, xmax, ymax))
+		return self.croped
 
