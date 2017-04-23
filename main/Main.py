8c394fced20506c7a4da5b190890a92e3471f2a9
Thomas Frøysa <thomasdf@stud.ntnu.no>
2017-04-23 20:01:23 +0200

    /nets:

--- main/Main.py
+++ main/Main.py
@@ -11,25 +11,25 @@ random_pic_path = base_dir + '/datasets/object-detection-crowdai/147949837196306
 def train_deeper():
 	from learning.deepernet import DeeperNet
 	classifier = DeeperNet()
-	classifier.train_neural_network()
+	classifier.train_neural_network(None)
 
 
 def main():
 	import os
-	os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+	os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+	from learning.AlexNet import AlexNet
 	from learning.thomasnetv2 import ThomasNet
 	from learning.deepernet import DeeperNet
 	classifier = DeeperNet()
 	# classifier.train_neural_network()
-	#ThomasNet()
-	#AlexNet()
 
-	from learning.AlexNet import AlexNet
-	a = ArrayTool.out(Image.open(random_pic_path), ThomasNet(), 3, 0.552)
+	a = ArrayTool.out(Image.open(random_pic_path), classifier, 1, 0.580)
 	Image.fromarray(a).show()
+
 
 if __name__ == '__main__':
 	# import multiprocessing
 	#
 	# multiprocessing.freeze_support()
-	train_deeper()
+	#train_deeper()
+	main()
