a9005a65e9708669d694ba532faa976199e87236
Thomas Frøysa <thomasdf@stud.ntnu.no>
2017-04-23 17:01:47 +0200

    use tf.layers instead of old-school things

--- learning/thomasnetv2.py
+++ learning/thomasnetv2.py
@@ -7,37 +7,32 @@ import time
 
 import os
 
-
 from load import labels
 from load.LoadProcess import LoadProcess
 from load.MainLoader import MainLoader
 
+
 class ThomasNet():
-####################################################################
-###                                 Helpers                      ###
-####################################################################
+	####################################################################
+	###                                 Helpers                      ###
+	####################################################################
 	def conv2D(self, x, W):
 		return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding="SAME")
 
 	def maxpool2d(self, x):
 		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-
-	def print_batch_info(self, batch_num, num_batches):
-		print("Batch ", batch_num, " of ", num_batches, " complete.")
-
+	def print_batch_info(self, batch_num, num_batches, time):
+		print("Batch ", batch_num, " of ", num_batches, " complete. Time: ", "{: 1.2f}".format(time))
 
 	def print_epoch_info(self, epoch_num, num_epochs, loss):
 		print("Epoch ", epoch_num, " of ", num_epochs, " complete. loss: ", loss)
 
-
 	def print_acc_info(self, accuracy):
 		print("accuracy: ", accuracy)
 
-
 	def print_acc_progress(self, percentage):
 		print("calculating accuracy. ", percentage, "% complete.")
-
 
 	def accuracy(self, prediction):
 		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y, 1))
@@ -46,54 +41,84 @@ class ThomasNet():
 		for i in range(self.num_test_batches):
 			test_x, test_y = self.loader.next_batch(self.batch_size, is_training=False)
 			accumulated_acc += acc.eval({self.x: test_x, self.y: test_y})
-			if i % max((self.num_test_batches // 10),1) == 0:
+			if i % max((self.num_test_batches // 10), 1) == 0:
 				self.print_acc_progress(i * 10)
 		self.print_acc_info(accumulated_acc / self.num_test_batches)
 		self.loader.index_test = 0
 		return accumulated_acc
 
-####################################################################
-###                               NN-Definition                  ###
-####################################################################
+	####################################################################
+	###                               NN-Definition                  ###
+	####################################################################
 
 	def neural_network_model(self, x, is_training: bool = True):
 		"""Defines the neural network model. Output is a n_labels long array"""
 
-		# define variables for layers (that is: allocate memory, create a structure)
-		weights = {"conv1": tf.Variable(tf.random_normal([5, 5, 1, 64])),
-		           "conv2": tf.Variable(tf.random_normal([5, 5, 64, 128])),
-		           "fc": tf.Variable(tf.random_normal([int(self.size / 4 * self.size / 4 * 128), 4096])),
-		           "out": tf.Variable(tf.random_normal([4096, self.n_classes]))
-		           }
+		# input layer
+		input = tf.reshape(x, shape=[-1, int(self.size), int(self.size), 1])
 
-		biases = {"conv1": tf.Variable(tf.random_normal([64])),
-		          "conv2": tf.Variable(tf.random_normal([128])),
-		          "fc": tf.Variable(tf.random_normal([4096])),
-		          "out": tf.Variable(tf.random_normal([self.n_classes]))
-		          }
+		# convolutional layer 1
+		conv1 = tf.layers.conv2d(
+			inputs=input,
+			filters=64,
+			kernel_size=5,
+			padding="same",
+			activation=tf.nn.relu,
+		)
 
-		x = tf.reshape(x, shape=[-1, self.size, self.size, 1])
+		# max-pooling layer 1
+		pool1 = tf.layers.max_pooling2d(
+			inputs=conv1,
+			pool_size=2,
+			strides=2,
+		)
 
-		conv1 = tf.nn.relu(self.conv2D(x, weights["conv1"]) + biases["conv1"])
-		conv1 = self.maxpool2d(conv1)
+		# convolutional layer 2
+		conv2 = tf.layers.conv2d(
+			inputs=pool1,
+			filters=128,  # output: [batchsize, input, input, 64]
+			kernel_size=5,
+			padding="same",  # output of same size as input
+			activation=tf.nn.relu,
+		)  # [batchsize, 14, 14, 64]
 
-		conv2 = tf.nn.relu(self.conv2D(conv1, weights["conv2"]) + biases["conv2"])
-		conv2 = self.maxpool2d(conv2)
+		# pooling layer 2
+		pool2 = tf.layers.max_pooling2d(
+			inputs=conv2,
+			pool_size=2,
+			strides=2,
+		)  # [batchsize, 7, 7, 64]
 
-		fc = tf.reshape(conv2, [-1, int(self.size / 4 * self.size / 4 * 128)])
-		fc = tf.nn.relu(tf.matmul(fc, weights["fc"]) + biases["fc"])
+		# flatten:
+		pool2flattened = tf.reshape(pool2, [-1, int(self.size / 4 * self.size / 4 * 128)])
 
-		# fc = tf.nn.dropout(fc, keep_rate)
+		# fully connected layer:
+		fc = tf.layers.dense(
+			inputs=pool2flattened,
+			units=4096,
+			activation=tf.nn.relu,
+		)
 
-		output = tf.matmul(fc, weights["out"]) + biases["out"]
+		# add dropout
+		dropout = tf.layers.dropout(
+			inputs=fc,
+			rate=self.dropout_rate,
+			training=is_training,
+		)  # [batchsize, 1024]
 
-		return output
+		logits = tf.layers.dense(
+			inputs=dropout,
+			units=self.n_classes,
+		)
 
-####################################################################
-###                               Training                       ###
-####################################################################
+		return logits
 
-	def train_neural_network(self, x):
+	####################################################################
+	###                               Training                       ###
+	####################################################################
+
+	def train_neural_network(self):
+		x = self.x
 		accs = []
 		epochs = []
 		print("start neural network training")
@@ -113,15 +138,15 @@ class ThomasNet():
 
 		batch_lognum = 10  # batch printing/batch modulo
 		epoch_lognum = 1  # epoch info interval
-		epoch_save_modulo = 1 #save interval
-		plot_modulo = 5 #plot interval
+		epoch_save_modulo = 1  # save interval
+		plot_modulo = 5  # plot interval
 
 		with tf.Session() as sess:
 			sess.run(init)
 			saver = tf.train.Saver()
 			t_total = time.time()
 
-			self.accuracy(logits) #test accuracy of random network
+			self.accuracy(logits)  # test accuracy of random network
 
 			for epoch in range(self.num_epochs):
 				epoch_loss = 0
@@ -140,8 +165,7 @@ class ThomasNet():
 
 					epoch_loss += c
 					if (batch_num % batch_lognum == 0):
-						self.print_batch_info(batch_num, self.num_train_batches)
-						print(batch_lognum, 'batches took', '{:10.3f}'.format(time.time() - t_total), 'seconds')
+						self.print_batch_info(batch_num, self.num_train_batches, time.time() - t_total)
 						t_total = time.time()
 
 				if epoch % epoch_lognum == 0:
@@ -154,8 +178,9 @@ class ThomasNet():
 				epochs.append(epoch)
 
 				if epoch % epoch_save_modulo == 0:
-					saver.save(sess, self.base_dir + "/savedmodels/thomasnet/epoch" + str(epoch) + "acc" + "{:1.3f}".format(
-						acc) + ".checkpoint")
+					saver.save(sess,
+					           self.base_dir + "/savedmodels/thomasnet/epoch" + str(epoch) + "acc" + "{:1.3f}".format(
+						           acc) + ".checkpoint")
 
 				if epoch % plot_modulo == 0 and epoch != 0:
 					plt.figure()
@@ -170,9 +195,9 @@ class ThomasNet():
 			plt.legend()
 			plt.show()
 
-####################################################################
-###                               Running                        ###
-####################################################################
+		####################################################################
+		###                               Running                        ###
+		####################################################################
 
 	def run_nn(self, batch, epoch, acc):
 		"""Runs a pre-trained network. x is a flattened image of the same size as the model has been trained"""
@@ -188,12 +213,12 @@ class ThomasNet():
 
 			init = tf.global_variables_initializer()
 			sess.run(init)
-			res = sess.run(tf.nn.softmax(logits), feed_dict={self.x: batch})
+			res = sess.run(logits, feed_dict={self.x: batch})
 		return res
 
-####################################################################
-###                               class-things                   ###
-####################################################################
+	####################################################################
+	###                               class-things                   ###
+	####################################################################
 
 	def __init__(self):
 
@@ -203,13 +228,13 @@ class ThomasNet():
 		self.base_dir = os.path.dirname(os.path.dirname(__file__))
 
 		# Variables
-		#classifier
+		# classifier
 		self.size = 32  # (X * X size)
 		self.num_epochs = 10
 		self.dropout_rate = 0.2
-		self.lr = 1e-5
+		self.lr = 1e-6
 
-		#loader
+		# loader
 		self.batch_size = 500
 		self.image_load_size = 25
 		self.test_set_rate = 0.05  # fraction of dataset used as test-set
@@ -229,11 +254,12 @@ class ThomasNet():
 		# data things
 
 		self.test_size = len(self.loader.data) * (self.test_set_rate)
-		self.num_train_batches = int((np.ceil(len(self.loader.trainindexes) / self.image_load_size))* self.dataset_fraction)
-		self.num_test_batches = max(int((np.ceil(len(self.loader.testindexes) / self.batch_size)) * self.dataset_fraction), 1)
+		self.num_train_batches = int(
+			(np.ceil(len(self.loader.trainindexes) / self.image_load_size)) * self.dataset_fraction)
+		self.num_test_batches = max(
+			int((np.ceil(len(self.loader.testindexes) / self.batch_size)) * self.dataset_fraction), 1)
 		# training things
 
 		# tensorflow things
 		self.x = tf.placeholder("float", [None, self.size * self.size])
 		self.y = tf.placeholder("float")
-		self.train_neural_network(self.x)
